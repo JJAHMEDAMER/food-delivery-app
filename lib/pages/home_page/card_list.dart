234dd55eeba3f9220a38b1b21f48f629ce88e542
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-09-20 19:49:00 +0200

    use shrinkwrap

--- lib/pages/home_page/card_list.dart
+++ lib/pages/home_page/card_list.dart
@@ -28,66 +28,66 @@ class _CardListState extends State<CardList> {
             ],
           ),
           // ListView Builder
-          Container(
-            height: 700,
-            // List View Builder Parent show have a given height
-            child: ListView.builder(
-              //Disable Scroll for the list
-              physics: NeverScrollableScrollPhysics(),
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return Container(
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center, // Default Behavior but anyways
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(top: Dimensions.height15),
-                        height: Dimensions.listImage,
-                        width: Dimensions.listImage,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(Dimensions.rad15),
-                          color: AppColors.mainColor,
-                          image: DecorationImage(
-                            fit: BoxFit.cover,
-                            image: AssetImage('assets/image/food0.png'),
-                          ),
+          ListView.builder(
+            //Disable Scroll for the list
+            physics: NeverScrollableScrollPhysics(),
+            shrinkWrap: true, // No Need for container
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return Container(
+                child: Row(
+                  crossAxisAlignment:
+                      CrossAxisAlignment.center, // Default Behavior but anyways
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: Dimensions.height15),
+                      height: Dimensions.listImage,
+                      width: Dimensions.listImage,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(Dimensions.rad15),
+                        color: AppColors.mainColor,
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage('assets/image/food0.png'),
                         ),
                       ),
-                      Expanded(
-                        // Expand the Container Width 
-                        child: Container(
-                          padding: EdgeInsets.all(Dimensions.height10),
-                          margin: EdgeInsets.only(top: Dimensions.height15), // must have matching margin with the image container to properly algin
-                          height: Dimensions.listText,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.only(
-                              topRight: Radius.circular(Dimensions.rad15),
-                              bottomRight: Radius.circular(Dimensions.rad15),
-                            ),
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              AppText(text: 'Nutritious Fruit Meal in China'),
-                              //SizedBox(height: Dimensions.height10),
-                              AppTextSmall(text: 'With chinese charcteristics'),
-                              //SizedBox(height: Dimensions.height10),
-                              IconTextWidget(
-                                  text: 'Normal',
-                                  distance: '17Km',
-                                  time: '32min'),
-                            ],
+                    ),
+                    Expanded(
+                      // Expand the Container Width
+                      child: Container(
+                        padding: EdgeInsets.all(Dimensions.height10),
+                        margin: EdgeInsets.only(
+                            top: Dimensions
+                                .height15), // must have matching margin with the image container to properly algin
+                        height: Dimensions.listText,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                            topRight: Radius.circular(Dimensions.rad15),
+                            bottomRight: Radius.circular(Dimensions.rad15),
                           ),
                         ),
-                      )
-                    ],
-                  ),
-                );
-              },
-            ),
-          )
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            AppText(text: 'Nutritious Fruit Meal in China'),
+                            //SizedBox(height: Dimensions.height10),
+                            AppTextSmall(text: 'With chinese charcteristics'),
+                            //SizedBox(height: Dimensions.height10),
+                            IconTextWidget(
+                                text: 'Normal',
+                                distance: '17Km',
+                                time: '32min'),
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              );
+            },
+          ),
         ],
       ),
     );
