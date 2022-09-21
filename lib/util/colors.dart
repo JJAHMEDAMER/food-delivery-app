69d68d797d67f83f300acd30becdcc77cca62164
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-09-21 22:54:57 +0200

    Change buttonBackaroundcolor

--- lib/util/colors.dart
+++ lib/util/colors.dart
@@ -10,7 +10,7 @@ class AppColors {
   static final Color iconcolor1 = const Color(0xFFffd28d);
   static final Color iconColor2 = const Color(0xFFfcab88);
   static final Color paraColor = const Color(0xFF8f837f);
-  static final Color buttonBackaroundcolor = Color.fromARGB(255, 216, 216, 216);
+  static final Color buttonBackaroundcolor = Color.fromARGB(255, 231, 231, 231);
   static final Color signColor = const Color(0xEFa2a22f);
   static final Color titleColor = const Color(0xFF5c524f);
   static final Color mainBlackColor = const Color(0xFF332d2b);
