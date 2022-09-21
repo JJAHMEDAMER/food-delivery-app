b4d01e3787b46c9cf6afd6cecaf6affb5b7ef85d
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-09-21 21:50:13 +0200

    fix Typo

--- lib/util/colors.dart
+++ lib/util/colors.dart
@@ -15,7 +15,7 @@ class AppColors {
   static final Color titleColor = const Color(0xFF5c524f);
   static final Color mainBlackColor = const Color(0xFF332d2b);
   // static final Color yellowColor = const Color ( 0xFFfa7552 ) ;
-  static final Color vellowColor = const Color(0xFFffd372);
+  static final Color yellowColor = const Color(0xFFffd372);
   static final Color shadow = const Color(0xFFe8e8e8);
 
   // Colors For Top Icon Bar
